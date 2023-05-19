a7bb8a2791a23e4f9dbc110ae5f9bf412ffff1d9
Carlos Baño <carlos.bano@epn.edu.ec>
2023-05-19 17:48:05 -0500

    Update

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask_script import Manager, Server
 from app import initialize_app
 from config import config
@@ -7,7 +8,8 @@ setting = config['development']
 app = initialize_app(setting)
 
 manager = Manager(app)
-manager.add_command('runserver', Server(host='0.0.0.0', port=5000))
+manager.add_command('runserver', Server(host='0.0.0.0'))
 
 if __name__ == '__main__':
-    manager.run()
+    port = int(os.environ.get('PORT', 5000))
+    manager.run(host='0.0.0.0', port=port)
